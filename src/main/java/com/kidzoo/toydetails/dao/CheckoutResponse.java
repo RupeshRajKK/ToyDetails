519f2382c372f2c9e215f7130e2ab869711fb9cf
RupeshRajKK <rupeshraj96@gmail.com>
2023-12-29 18:37:42 +0530

    Revert "Removed json mapping annotations"
    This reverts commit 97ce5fed4ba9d9784ee7e74d5d42acb3d5c44104.

--- src/main/java/com/kidzoo/toydetails/dao/CheckoutResponse.java
+++ src/main/java/com/kidzoo/toydetails/dao/CheckoutResponse.java
@@ -1,5 +1,9 @@
 package com.kidzoo.toydetails.dao;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.kidzoo.toydetails.client.entity.Basket;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,9 +13,13 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 
 public class CheckoutResponse {
+    @JsonProperty("T")
     public double total;
+    @JsonProperty("I")
     public List<CheckoutItem> items;
 
     CheckoutResponse(List<CheckoutItem> items){
